7483b3546e95566f46f812096987cf974a8716ff
E-Klauber <eklauber42@gmail.com>
2022-07-23 16:28:07 -0400

    added assignment code

---
+++ Basic Data Types Activity/Basic_Data_Types.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = 'Enter the forge.'
+let name = 'Zelcor'
+let job = 'Master bladesmith'
+let location = 'Kingdom of Havernith'
+let employer = 'King Hadrian'
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let century = 11
+let timePeriod = 1089
+let age = 49
+let weaponNum = 1200
+let yearsSmithing = 36
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let toolsOwned = ['forging hammer','anvil','forge', 'tongs', 'bellows', 'chisel' ]
+let gearUsed = ['woolen aprons', 'thick gloves', 'leather boots', 'woolen caps']
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let losingWar = true;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+ 
+console.log(greetings + " "+"I am " + name + ".");
+console.log("I am a "+ job + " "+ "employed by " + employer + ".");
+console.log("I work in the "+location+".");
+console.log("I have forged mighty weapons since I first was an apprentice, for "+yearsSmithing+" years and have forged "+weaponNum+" blades.");
+console.log("Now in the "+century+"th century, year "+ timePeriod + ", I forge weapons for the Kingdom's infantry and knights.");
+console.log("In my work, I use tools such as a "+toolsOwned[0]+", "+toolsOwned[1]+", a "+toolsOwned[2]+", and "+toolsOwned[4]+" to craft my weapons.");
+console.log("After forging my weapons, I use a "+toolsOwned[5]+", and "+toolsOwned[3]+" to finish my weapons.");
+console.log("Blacksmithing is a dangerous craft, and so I use "+gearUsed[0]+", "+gearUsed[1]+", "+gearUsed[2]+", and "+gearUsed[3]+" to protect myself from burns and injuries.");
+console.log("Why are you here?");
+console.log("Me: How goes the war against the Haldreds, I have heard we are losing.")
+console.log("It is "+losingWar+", our men are under equipped and no matter how fast I produce blades, myself and the few blacksmiths in the kingdom cannot keep up with the army's demand.");
+console.log("Me: Could I join your forge as an apprentice and learn the art of blacksmithing?");
+console.log("It is a long and hard road towards becoming a bladesmith, but I have faith in your efforts and your aility to help me speed up production, so I accept you as my apprentice.");
